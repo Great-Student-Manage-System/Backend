6a5913777221f7726814d31c8e21ab4317f42450
Snanny-Hyun <kimlisa0819@gmail.com>
2022-09-20 19:55:02 +0900

    StudentRecordService 수정
    구현체 수정 및 year localdate 적용

--- src/main/java/com/example/demo/service/StudentRecordService.java
+++ src/main/java/com/example/demo/service/StudentRecordService.java
@@ -1,13 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Record;
+import com.example.demo.model.RecordUpdateDto;
 import com.example.demo.model.Subject;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface StudentRecordService {
-    List<Record> getStudentRecords(int studentId, Subject subject, Date year);
+    List<Record> getStudentRecords(int studentId, String subject, LocalDate year);
     void addStudentRecord(Record record);
-    void updateStudentRecord(Record record);
+    void updateStudentRecord(RecordUpdateDto recordUpdateDto);
 }
