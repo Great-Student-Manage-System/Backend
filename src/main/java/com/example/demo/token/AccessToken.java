fb698511a9dcf8e5624c1ab0167bbd4990674984
robinjoon <robin980108@naver.com>
2022-09-24 23:09:39 +0900

    Refactor: 토큰인증작업 구조 개선

--- src/main/java/com/example/demo/token/AccessToken.java
+++ src/main/java/com/example/demo/token/AccessToken.java
@@ -2,27 +2,14 @@ package com.example.demo.token;
 
 import com.example.demo.exception.SystemException;
 import com.example.demo.model.ErrorMessage;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter(AccessLevel.PACKAGE)
 class AccessToken {
     private String tokenString;
-
-    AccessToken(int id){
-
-    }
-    int getId(){
-        if(isValid()){
-            return 1; //Todo 구현 필요
-        }else {
-            ErrorMessage errorMessage = ErrorMessage.builder()
-                    .message("엑세스 토큰이 만료되었습니다.")
-                    .code(401)
-                    .build();
-            throw new SystemException(errorMessage);
-        }
-    }
-    boolean isValid(){
-        return false;
-    }
 }
--- src/main/java/com/example/demo/token/RefreshToken.java
+++ src/main/java/com/example/demo/token/RefreshToken.java
@@ -1,17 +1,12 @@
 package com.example.demo.token;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 @NoArgsConstructor
+@AllArgsConstructor
 class RefreshToken {
+    @Getter(AccessLevel.PACKAGE)
     private String tokenString;
-
-    RefreshToken(int id){
-
-    }
-    boolean isValid(){
-        return false;
-    }
-    int getId(){
-        return 1;
-    }
 }
