938f73330c86c295805c82e70236fddf9511e2f6
robinjoon <robin980108@naver.com>
2022-09-24 21:00:45 +0900

    Fix: JoinService 필요없는 메서드 제거

--- src/main/java/com/example/demo/service/JoinService.java
+++ src/main/java/com/example/demo/service/JoinService.java
@@ -6,10 +6,6 @@ public interface JoinService {
     void checkEmail(Email email);
     void createEmailCode(Email email);
     void certEmail(Email email,String code);
-
-    Token login(Email email, Password password);
-    void join(Teacher teacher);
-    Token renewalToken(Token token);
     void updateTeacherNickname(Teacher teacher, String nickname);
     void updateTeacherPassword(UpdtatePasswordDto passwordDto);
     Teacher getTeacher(int teacherId);
