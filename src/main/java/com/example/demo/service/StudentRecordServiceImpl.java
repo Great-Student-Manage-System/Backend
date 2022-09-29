d8365f52c2434bab011120f15fbe3ac2768e408c
Snanny-Hyun <kimlisa0819@gmail.com>
2022-09-29 23:38:02 +0900

    StudentRecordsServiceImpl
    학생 성적 기록

--- src/main/java/com/example/demo/service/StudentRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/StudentRecordServiceImpl.java
@@ -1,15 +1,15 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Record;
-import com.example.demo.model.RecordSelectDto;
-import com.example.demo.model.RecordUpdateDto;
-import com.example.demo.model.Subject;
+import com.example.demo.exception.SystemException;
+import com.example.demo.model.*;
 import com.example.demo.repository.RecordRepository;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.ErrorManager;
 
 @Service
 public class StudentRecordServiceImpl implements StudentRecordService{
@@ -23,7 +23,16 @@ public class StudentRecordServiceImpl implements StudentRecordService{
 
     @Override
     public void addStudentRecord(Record record) {
-        recordRepository.save(record);
+        try{
+            recordRepository.save(record);
+        }catch (Exception e){
+            //sql문에서 유니크를 중복으로 던지면 생기는 에러
+            ErrorMessage errorMessage = ErrorMessage.builder()
+                    .code(400)
+                    .message("이미 해당 학생의 성적이 등록된 시험입니다.")
+                    .build();
+            throw new SystemException(errorMessage);
+        }
     }
 
     @Override
@@ -31,7 +40,12 @@ public class StudentRecordServiceImpl implements StudentRecordService{
         try{
             recordRepository.update(recordUpdateDto);
         }catch(Exception e){
-
+            ErrorMessage errorMessage = ErrorMessage.builder()
+                    .code(404)
+                    .message("아직 등록된 적이 없는 시험입니다.")
+                    .method(HttpMethod.PATCH)
+                    .build();
+            throw new SystemException(errorMessage);
         }
     }
 }
