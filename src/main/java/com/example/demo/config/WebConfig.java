6c3ecd64464228efd9876a9d8cb8b02cc3f69933
robinjoon <robin980108@naver.com>
2022-10-01 19:09:59 +0900

    Feat: DB 연결 설정 추가

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
+    }
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource);
+        return tm;
+    }
+//    @Bean
+//    public AdminCertInterceptor adminCertInterceptor(){
+//        return new AdminCertInterceptor();
+//    }
+}
