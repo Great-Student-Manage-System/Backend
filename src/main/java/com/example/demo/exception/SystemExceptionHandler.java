3930e8d12ec71472bc299ffc051faec6a77293ff
robinjoon <robin980108@naver.com>
2022-10-15 20:42:05 +0900

    Fix: SystemExceptionHandler 에러 메시지 수정

--- src/main/java/com/example/demo/exception/SystemExceptionHandler.java
+++ src/main/java/com/example/demo/exception/SystemExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.demo.exception;
 
 import com.example.demo.model.dto.response.ErrorMessage;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +24,7 @@ public class SystemExceptionHandler {
         e.printStackTrace();
         ErrorMessage errorMessage = e.getErrorMessage();
         errorMessage.setPath(request.getRequestURI());
+        errorMessage.setMethod(HttpMethod.valueOf(request.getMethod()));
         int status = errorMessage.getCode();
         return ResponseEntity.status(status).body(errorMessage);
     }
