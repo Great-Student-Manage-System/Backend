709ee40e6c44f8113a1c4ab05e4f4e44f3e132d3
robinjoon <robin980108@naver.com>
2022-10-15 23:24:38 +0900

    Fix: 누락된 PostMapping 추가

--- src/main/java/com/example/demo/controller/AddStudentController.java
+++ src/main/java/com/example/demo/controller/AddStudentController.java
@@ -6,13 +6,16 @@ import com.example.demo.model.dto.response.SelectStudentsResponseDto;
 import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddStudentController {
     @Autowired
-    private static StudentService studentService;
-    public ResponseEntity<ResponseDto<?>> addStudent(AddStudentDto dto){
+    private StudentService studentService;
+    @PostMapping("/api/students")
+    public ResponseEntity<ResponseDto<?>> addStudent(@RequestBody AddStudentDto dto){
         int teacherId = 1;
         studentService.addStudent(teacherId,dto);
         return ResponseEntity.ok(ResponseDto.builder().code(200).response("학생 추가에 성공했습니다").build());
