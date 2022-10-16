9769809b407db66e048a73572bdf27be9f244c92
robinjoon <robin980108@naver.com>
2022-10-16 20:16:58 +0900

    Fix: AccessToken에서 id 추출하도록 수정

--- src/main/java/com/example/demo/controller/SelectStudentsController.java
+++ src/main/java/com/example/demo/controller/SelectStudentsController.java
@@ -3,10 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.model.dto.response.ResponseDto;
 import com.example.demo.model.dto.response.SelectStudentsResponseDto;
 import com.example.demo.service.StudentService;
+import com.example.demo.token.AccessToken;
+import com.example.demo.token.TokenManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -15,10 +18,13 @@ import java.util.Collections;
 public class SelectStudentsController {
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private TokenManager tokenManager;
     @GetMapping("/api/students/{page}")
-    public ResponseEntity<ResponseDto<?>> getStudentList(@PathVariable int page){
-        int teacherID = 1;
-        SelectStudentsResponseDto dto = studentService.getStudents(teacherID,page);
+    public ResponseEntity<ResponseDto<?>> getStudentList(@RequestHeader("Authorization") String accessTokenString, @PathVariable int page){
+        AccessToken accessToken = new AccessToken(accessTokenString);
+        int teacherId = tokenManager.getIdFromAccessToken(accessToken);
+        SelectStudentsResponseDto dto = studentService.getStudents(teacherId,page);
 
         ResponseDto<SelectStudentsResponseDto> responseDto = ResponseDto.<SelectStudentsResponseDto>builder()
                 .code(200)
