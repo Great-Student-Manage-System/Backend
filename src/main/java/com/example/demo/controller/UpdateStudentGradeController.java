8fefbbb989e650449064f04cb8f9d0d285c58584
Snanny-Hyun <kimlisa0819@gmail.com>
2022-10-18 19:04:32 +0900

    학생 성적 수정 컨트롤러

---
+++ src/main/java/com/example/demo/controller/UpdateStudentGradeController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.dto.request.AddRecordDto;
+import com.example.demo.model.dto.request.UpdateRecordDto;
+import com.example.demo.model.dto.response.ResponseDto;
+import com.example.demo.service.StudentRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UpdateStudentGradeController {
+    @Autowired
+    private StudentRecordService studentRecordService;
+
+    @PatchMapping("/api/studests/{studentId}")
+    public ResponseEntity<ResponseDto<?>> updateStudentRecord(@RequestBody UpdateRecordDto dto){
+        studentRecordService.updateStudentRecord(dto);
+        return ResponseEntity.ok(ResponseDto.builder().code(200).build());
+    }
+
+}
